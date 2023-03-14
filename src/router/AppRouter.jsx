1c79aa51cc1521f965c4fa8553dce357746ff671
Erhan <erhantezer68@gmail.com>
2023-03-14 23:07:12 +0300

    router added

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
+import Navbar from '../components/navbar/Navbar'
+import Footer from '../components/footer/Footer'
+import Home from '../pages/home/Home'
+import About from '../pages/about/About'
+import Projects from '../pages/projects/Projects'
+import Contact from '../pages/contact/Contact'
+import NotFound from '../components/error/NotFound'
 
 const AppRouter = () => {
   return (
-    <div>AppRouter</div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={
+        <>
+          <Navbar/>
+          <Outlet/>
+          <Footer/>
+        </>
+        }>
+          <Route path='/' element={<Home/>} />
+          <Route path='about' element={<About/>} />
+          <Route path='projects' element={<Projects/>} />
+          <Route path='contacts' element={<Contact/>} />
+        </Route>
+        <Route path='*' element={<NotFound/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
-
+//! navbar ve footerın notfound sayfasında gözükmesini istemediğimiz için yukarıdaki yapıyı kurduk
 export default AppRouter
