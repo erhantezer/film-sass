bedd26c390fd3bfec42e88269954f4704e180599
Erhan <erhantezer68@gmail.com>
2023-03-15 23:09:43 +0300

    router.js updated

--- src/router/router.js
+++ src/router/router.js
@@ -1,9 +1,41 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Outlet, createBrowserRouter } from "react-router-dom";
+import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/footer/Footer";
+import NotFound from "../components/error/NotFound";
+import Home from "../pages/home/Home";
+import About from "../pages/about/About";
+import Projects from "../pages/projects/Projects";
+import Contact from "../pages/contact/Contact";
 
 
 
 export const router = createBrowserRouter([
     {
-        element: ()
-    }
-])
+        element: (
+            <>
+                <Navbar />
+                <Outlet />
+                <Footer />
+            </>
+        ),
+        errorElement: <NotFound/>,
+        children: [
+            {
+                path: "/",
+                element: <Home/>,
+            },
+            {
+                path: "/about",
+                element: <About/>
+            },
+            {
+                path: "/projects",
+                element: <Projects/>,
+            },
+            {
+                path: "/contacts",
+                element: <Contact/>
+            },
+        ],
+    },
+]);
