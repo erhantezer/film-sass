26eb352b6ae26133f96f926decf3a319633e1412
Erhan <erhantezer68@gmail.com>
2023-03-16 11:25:38 +0300

    updated

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
+import Loading from '../../components/Loading';
 
 const Projects = () => {
+  const ProjectCards = lazy(() => import("../../components/ProjectCards"));
+
+
+  // <Suspense fallback={<div>Loadings.....</div>}>
+  //   <ProjectCards />
+  // </Suspense>
+  //! return içine bunuda yazabiliriz
+
   return (
-    <div>Projects</div>
+    
+    <>
+      <div className='project-bgImg-container'>
+        <div className='projects__bio-image'>
+          <h1>My Projects</h1>
+        </div>
+      </div>
+      <main>
+        <div>
+          <Suspense fallback={<Loading />}> 
+            <ProjectCards/>
+          </Suspense>
+        </div>
+      </main>
+    </>
   )
 }
 
