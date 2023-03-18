6c932fd2c017d20aed0ec9bfdfa6ed9af9a1d038
Erhan <erhantezer68@gmail.com>
2023-03-18 11:32:01 +0300

    updated app js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import AppRouter from "./router/AppRouter";
+import "./App.scss";
 // import { RouterProvider } from "react-router-dom";
 // import { router } from "./router/router";
-import "./App.scss"
 
 function App() {
   return (
