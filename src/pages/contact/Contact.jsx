027b52b32afceda2ecdb98b43ea2d6b2644b5b5a
Erhan <erhantezer68@gmail.com>
2023-03-19 16:49:38 +0300

    updated project and finished

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
         <div className="contact__list">
           <div className="contact__item">
             <i className="fas fa-envelope"></i> Email
-            <div className="text-secondary">wednesday@nevermoreacademy.edu</div>
+            <div className="text-secondary">wednesday@academy.edu</div>
           </div>
           <div className="contact__item">
             <i className="fas fa-mobile-alt"></i> Phone
@@ -17,7 +17,7 @@ const Contact = () => {
           <div className="contact__item">
             <i className="fas fa-map-marker-alt"></i> Address
             <div className="text-secondary">
-              Strada Zamorei 1, Bușteni 105500, Romania
+              Strada Zamorei 1 Romania
             </div>
           </div>
         </div>
