8347db051ad7e432a8abf5690533af0e982fadb1
Erhan <erhantezer68@gmail.com>
2023-03-27 09:06:33 +0300

    updated

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -3,25 +3,31 @@ import "./Contact.scss"
 const Contact = () => {
   return (
     <div className="contact-bgImg-container">
+
       <main className="contact">
         <h2>Contact Me ...</h2>
         <div className="contact__list">
+
           <div className="contact__item">
             <i className="fas fa-envelope"></i> Email
             <div className="text-secondary">wednesday@academy.edu</div>
           </div>
+
           <div className="contact__item">
             <i className="fas fa-mobile-alt"></i> Phone
             <div className="text-secondary">+40 (571) 360-1234</div>
           </div>
+
           <div className="contact__item">
             <i className="fas fa-map-marker-alt"></i> Address
             <div className="text-secondary">
               Strada Zamorei 1 Romania
             </div>
           </div>
+          
         </div>
       </main>
+
     </div>
   )
 }
